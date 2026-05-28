3a975c816470dc65a6e94dab820fa13b917f2cea
tho493 <chitho040903@gmail.com>
2026-05-28 15:29:27 +0700

    Fix block Mixed-content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+            $proto = (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ? 'https' : 'http';
+            config(['app.url' => $proto . '://' . $_SERVER['HTTP_X_FORWARDED_HOST']]);
+        } elseif (isset($_SERVER['HTTP_HOST'])) {
+            $proto = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https' : 'http';
+            config(['app.url' => $proto . '://' . $_SERVER['HTTP_HOST']]);
+        }
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'check_cart' => \App\Modules\Core\Http\Middleware\CheckShoppingCartStatus::class,
             'admin_auth' => \App\Modules\Core\Http\Middleware\AdminAuthenticate::class,
